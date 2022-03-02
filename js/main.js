559045cf69d26417137cc23c318b31894a2125d9
JBragaV <jocimarcaiadobraga@gmail.com>
2022-03-01 23:40:04 -0300

    Commit de casa

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-let botons = document.getElementsByClassName("tecla")
+/*let botons = document.getElementsByClassName("tecla")
 console.log(botons)
 let telefone = document.querySelector("input[type=tel]")
 console.log(telefone)
+*/
+function tocaSons(instrumento){
+    const texto = instrumento;
+    const idAudio = `#som_${texto}`;
+    let alvo = document.querySelector(idAudio);
+    alvo.play();
+    //alert("Vamos que vamos!!!!")
+};
+
+const botoes = document.querySelectorAll(".tecla");
+botoes.forEach(botao => {
+    const classeInstrumento = botao.classList[1];
+    botao.addEventListener("click", () => tocaSons(classeInstrumento));
+});
